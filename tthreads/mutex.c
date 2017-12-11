6bce4ff21cdcf80f6f60a2f479ca380d2752f523
Alex5Bal <abalderrama5@outlook.com>
2017-12-11 14:44:24 -0700

    Add files via upload

--- tthreads/mutex.c
+++ tthreads/mutex.c
@@ -10,13 +10,13 @@ void mutexLock(Mutex *m) {
   while (m->numAvailable == 0) {
     contextSwitch(&m->waitingThreads); /* block */
   }
-  m->numAvailable = 0;
+  m->numAvailable -= 1;
   yield();			/* just to mix things up a bit! */
 }
 
 void mutexUnlock(Mutex *m) {
   yield();			/* just to mix things up a bit! */
-  m->numAvailable = 1;
+  m->numAvailable += 1;
   if (m->waitingThreads.count != 0) /* make another thread ready */
     enqueueThread(&readyQ, dequeueThread(&m->waitingThreads));
   yield();			/* just to mix things up a bit! */
